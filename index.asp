1fe7682e90e0c806c7bf0240dcda59e32ea6b0a3
SuperMapiClient <maojingyun@supermap.com>
2012-03-02 14:28:14 +0800

    bev all files

---
+++ index.asp
@@ -0,0 +1,20 @@
+﻿<%
+            dim fname
+            fname=request.form("text")
+
+            If fname<>"" Then
+
+            
+
+            Set objStream = Server.CreateObject("ADODB.Stream")
+            With objStream
+            .Open
+            .Charset = "utf-8"
+            .Position = objStream.Size
+            .WriteText=fname
+            .SaveToFile server.mappath("index.html"),2 
+            .Close
+            End With
+            Set objStream = Nothing
+            End If
+         %>
---
+++ js/SuperMap.Include.js
@@ -0,0 +1,29 @@
+﻿{	
+	var r = new RegExp("(^|(.*?\\/))(SuperMap.Include\.js)(\\?|$)"),
+	s = document.getElementsByTagName('script'),
+	src, m, baseurl = "";
+	for(var i=0, len=s.length; i<len; i++) {
+		src = s[i].getAttribute('src');
+		if(src) {
+			var m = src.match(r);
+			if(m) {
+				baseurl = m[1];
+				break;
+			}
+		}
+	}
+    inputScript(baseurl+'OpenLayers.js');
+    inputScript(baseurl+'SuperMap-6.1.js');
+    inputCSS('style.css');
+    inputCSS('ie6-style.css');
+    inputCSS('google.css');
+    inputCSS('framedCloud.css');
+}
+function inputScript(inc){
+    var script='<'+'script type="text/javascript" src="'+inc+'"'+'><'+'/script>'; 
+    document.writeln(script);
+}
+function inputCSS(style){
+        var css='<'+'link rel="stylesheet" href="'+baseurl+ '../resource/theme/default/'+style+'"'+'><'+'/>'; 
+    document.writeln(css);
+}
